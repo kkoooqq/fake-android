64b2162240d5bd3beddda582c38a6b6d685c1ec5
kkoooqq <>
2021-11-04 19:09:41 -0400

    android service initialize commit

---
+++ services/fakeandroid-service/app/src/main/java/kkoooqq/fakeandroid/service/MainActivity.java
@@ -0,0 +1,14 @@
+package kkoooqq.fakeandroid.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
